c82317006f93f37f9c6ea9e51c5891eb44a1c6d5
ehtesham015 <ehtesham2002@gmail.com>
2025-04-26 10:33:06 +0530

    Initial commit

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,16 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <h3>Interview Genration</h3>
+    <Agent userName = "You" userId="user1" type="generate"></Agent>
+    </>
+
+    
+    
+  )
+}
+
+export default page
---
+++ firebase/client.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDdVyW8o2_wRjmoELOKbiSRVH_21B2lYpY",
+  authDomain: "interviewprep-aee45.firebaseapp.com",
+  projectId: "interviewprep-aee45",
+  storageBucket: "interviewprep-aee45.firebasestorage.app",
+  messagingSenderId: "441553626078",
+  appId: "1:441553626078:web:1861fc5b96a2d92949734f",
+  measurementId: "G-CGN3JT2W34"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
