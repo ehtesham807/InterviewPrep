9443e00b3c645b2f99833f1436ab585cdc62f275
ehtesham015 <ehtesham2002@gmail.com>
2025-04-28 11:42:33 +0530

    implement interview generation

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -4,6 +4,11 @@ import { google } from "@ai-sdk/google";
 import { db } from "@/firebase/admin";
 import { getRandomInterviewCover } from "@/lib/utils";
 
+
+export async function GET() {
+  return Response.json({ success: true, data: "Thank you!" }, { status: 200 });
+}
+
 export async function POST(request: Request) {
   const { type, role, level, techstack, amount, userid } = await request.json();
 
@@ -44,8 +49,4 @@ export async function POST(request: Request) {
     console.error("Error:", error);
     return Response.json({ success: false, error: error }, { status: 500 });
   }
-}
-
-export async function GET() {
-  return Response.json({ success: true, data: "Thank you!" }, { status: 200 });
 }
--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,57 +1,62 @@
 "use client";
-
+import React from 'react'
 import Image from "next/image";
-// import { useState, useEffect } from "react";
-// import { useRouter } from "next/navigation";
 
 import { cn } from "@/lib/utils";
-// import { vapi } from "@/lib/vapi.sdk";
-// import { interviewer } from "@/constants";
-// import { createFeedback } from "@/lib/actions/general.action";
 
-enum callStatus {
-    INACTIVE = "INACTIVE",
-    CONNECTING = "CONNECTING",
-    ACTIVE = "ACTIVE",
-    FINISHED = "FINISHED",
-  }
+enum CallStatus{
+  INACTIVE = 'INACTIVE',
+  CONNECTING = 'CONNECTING',
+  ACTIVE = 'ACTIVE',
+  FINISHED = 'FINISHED',
+}
 
 const Agent = ({ userName }: AgentProps) => {
-    const isSpeaking = true
+  const callStatus = CallStatus.FINISHED
+  const isSpeaking = true;
+  const messages = [
+    "What your name?",
+    "My name is John Doe, nice to meet you!"
+  ]
+
+  const lastMessage = messages[messages.length-1]
+  
+
   return (
     <>
-    <div className="call-view">
-        {/* AI Interviewer Card */}
-        <div className="card-interviewer">
-          <div className="avatar">
-            <Image
-              src="/ai-avatar.png"
-              alt="profile-image"
-              width={65}
-              height={54}
-              className="object-cover"
-            />
-            {isSpeaking && <span className="animate-speak" />}
+      <div>
+        <div className="call-view">
+          {/* AI Interviewer Card */}
+          <div className="card-interviewer">
+            <div className="avatar">
+              <Image
+                src="/ai-avatar.png"
+                alt="profile-image"
+                width={65}
+                height={54}
+                className="object-cover"
+              />
+              {isSpeaking && <span className="animate-speak" />}
+            </div>
+            <h3>AI Interviewer</h3>
           </div>
-          <h3>AI Interviewer</h3>
+
+          {/* User Profile Card */}
+          <div className="card-border">
+            <div className="card-content">
+              <Image
+                src="/user-avatar.png"
+                alt="profile-image"
+                width={539}
+                height={539}
+                className="rounded-full object-cover size-[120px]"
+              />
+              <h3>{userName}</h3>
+            </div>
+          </div>
         </div>
 
-        {/* User Profile Card */}
-        <div className="card-border">
-          <div className="card-content">
-            <Image
-              src="/user-avatar.png"
-              alt="profile-image"
-              width={539}
-              height={539}
-              className="rounded-full object-cover size-[120px]"
-            />
-            <h3>{userName}</h3>
-          </div>
-        </div>
-      </div>
-
-    {/* //   {messages.length > 0 && (
+         {messages.length > 0 && (
         <div className="transcript-border">
           <div className="transcript">
             <p
@@ -65,13 +70,15 @@ const Agent = ({ userName }: AgentProps) => {
             </p>
           </div>
         </div>
-      )} */}
+      )}
 
 
 
       <div className="w-full flex justify-center">
-        {callStatus !== "ACTIVE" ? (
-          <button className="relative btn-call" onClick={() => handleCall()}>
+           
+
+      {callStatus !== "ACTIVE" ? (
+          <button className="relative btn-call">
             <span
               className={cn(
                 "absolute animate-ping rounded-full opacity-75",
@@ -86,15 +93,17 @@ const Agent = ({ userName }: AgentProps) => {
             </span>
           </button>
         ) : (
-          <button className="btn-disconnect" onClick={() => handleDisconnect()}>
+          <button className="btn-disconnect">
             End
           </button>
         )}
-      </div>
 
-      
+        
+      </div>
+        
+     </div>
     </>
-  )
-}
+  );
+};
 
 export default Agent
